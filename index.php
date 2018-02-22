2b86a7716954d51575b016b3906486423dfa0941
Yasuteru Ono <teruteruteru1@gmail.com>
2018-02-22 14:07:46 +0800

    index完成

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
     echo 'hello world';
-    
+    var_dump(1);
+
+    $name = 'Yasuteru Ono';
  ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+   <title></title>
+ </head>
+ <body>
+ <h1>hello github</h1>
+ </body>
+ </html>
